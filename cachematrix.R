30269c3c464f61556fac8aeed6ca6a135a5759ba
lyvonne0912 <lyvonne0912@gmail.com>
2015-05-24 12:28:16 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Yuk Yin Lai R Programming Assignment 2
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## the makeCacheMatrix function creates a special "matrix" object that can cache the inverse of the input matrix
+## the cacheSolve function figures the inverse of the special "matrix" returned by makeCacheMatrix.  If the inverse of the special matrix has already been cached, it would skip the computing part and return the inverse of the special "matrix" from cache
 
+makeCacheMatrix <- function(z = matrix()) {
+  n <- NULL
+  set <- function(y) { ##set the value of the matrix
+    z <<- y 
+    n <<- NULL
+  }
+  get <- function() z ##get the value of the matrix
+  setinv <- function(solve) n <<- solve ##set the value of the inverse
+  getinv <- function() n  ##get the value of the inverse
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## the cacheSolve function figures the inverse of the special "matrix" returned by makeCacheMatrix.  If the inverse of the special matrix has already been cached, it would skip the computing part and return the inverse of the special "matrix" from cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(z, ...) {
+  ## Return a matrix that is the inverse of the cached matrix z
+  n <- z$getinv()
+  if(!is.null(n)) { ## if inverse is already cached
+    message("getting cached data")
+    return(n)  ## will returned the cached data
+  }
+  data <- z$get() ## will retrieve the input matrix
+  n <- solve(data, ...) ##inverse the matrix #data #solve(data) %*% data #data[,1] #solve(data) %*% data #mean(data, ...)
+  z$setinv(n)
+  n
 }
